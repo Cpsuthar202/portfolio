2478d0b4083f8d18bc32c571e345c3d465babe13
Chandraprakashchauhan <chandraprakash.marwadtech@gmail.com>
2025-01-14 12:22:22 +0530

    target: 'esnext',

--- vite.config.js
+++ vite.config.js
@@ -6,29 +6,30 @@ var FRONTEND_PORT = process.env.VITE_APP_PORT;
 console.log("Port from env:", FRONTEND_PORT);
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react()],
-    base: "/",
-    optimizeDeps: {
-        exclude: ["chunk-EWS7HTKY.js"],
+  plugins: [react()],
+  base: "/",
+  optimizeDeps: {
+    exclude: ["chunk-EWS7HTKY.js"],
+  },
+  server: {
+    port: Number(FRONTEND_PORT) || 3002,
+    host: FRONTEND_HOST || "localhost",
+  },
+  build: {
+    outDir: "build",
+    target: "esnext",
+  },
+  preview: {
+    port: 3001,
+    host: FRONTEND_HOST || "localhost",
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src"),
+      "@image": path.resolve(__dirname, "src/assets/image/index"),
+      "@svg": path.resolve(__dirname, "src/assets/svg/index"),
+      "@color": path.resolve(__dirname, "src/theme/utils/color"),
+      "@components": path.resolve(__dirname, "src/components"),
     },
-    server: {
-        port: Number(FRONTEND_PORT) || 3002,
-        host: FRONTEND_HOST || "localhost",
-    },
-    build: {
-        outDir: "build",
-    },
-    preview: {
-        port: 3001,
-        host: FRONTEND_HOST || "localhost",
-    },
-    resolve: {
-        alias: {
-            "@": path.resolve(__dirname, "src"),
-            "@image": path.resolve(__dirname, "src/assets/image/index"),
-            "@svg": path.resolve(__dirname, "src/assets/svg/index"),
-            "@color": path.resolve(__dirname, "src/theme/utils/color"),
-            "@components": path.resolve(__dirname, "src/components"),
-        },
-    },
+  },
 });
